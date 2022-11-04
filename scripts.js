785fe4a82c652efa67c53a18c45469926e22f796
gryda <gryda@hismith.ru>
2022-11-04 21:41:23 +0300

    Убирает debuuger задержку

--- scripts.js
+++ scripts.js
@@ -93,7 +93,6 @@
     }
 
     document.addEventListener('DOMContentLoaded', () => {
-        debugger
         Array.from(document.querySelectorAll('.main__devices')).forEach(makeTabs);
         Array.from(document.querySelectorAll('.header__menu')).forEach(makeMenu);
     });
